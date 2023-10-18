8532d5fcf8af8be65af44a2e125a57379df0868e
HamrounAdel <HamrounAdel@email.fr>
2023-10-19 00:15:55 +0100

    first commit

--- src/component/movies/MovieList.js
+++ src/component/movies/MovieList.js
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MovieCard from './MovieCard'
 import './MovieList.css'
 import {  useSelector } from 'react-redux'
 function MovieList({inputTitle,inputRate}) {
   const movie=useSelector(state=>state.Movie)
+  const [sort,setSort]=useState('all')
   return (
+    <div>
+      <div>
+        <button type='button'  onClick={()=>setSort('all')} >ALL</button>
+        <button  type='button' onClick={()=>setSort('done')}>DONE</button>
+        <button  type='button' onClick={()=>setSort('notyet')}>NOTYET</button>
+        <hr/>
+      </div>
     <div className='grid-container'>
-      {movie.filter((el)=> (el.title.toLowerCase().includes(inputTitle.toLowerCase().trim()))
+      
+      { movie.length>0 && sort==='all'?
+      movie.filter((el)=> (el.title.toLowerCase().includes (inputTitle.toLowerCase().trim()))&& el.rate>=inputRate)
+      .map((el)=>  <MovieCard  el={el}/>):null}
+
+       { movie.length>0 && sort === 'done'?
+      movie.filter(el=>el.checked === true).filter((el)=>(el.title.toLowerCase().includes(inputTitle.toLowerCase().trim()))
       && el.rate>=inputRate)
-      .map((el)=>  <MovieCard  el={el}    />)}
+      .map((el)=><MovieCard  el={el}/>):null}
+
+       { movie.length>0 && sort === 'notyet'?
+      movie.filter(el=>el.checked === false).filter((el)=> (el.title.toLowerCase().includes(inputTitle.toLowerCase().trim()))
+      && el.rate>=inputRate)
+      .map((el)=><MovieCard  el={el}/>):null}
     
+    </div>
     </div>
   )
 }
--- src/redux/movieSlice.js
+++ src/redux/movieSlice.js
@@ -5,38 +5,45 @@ export const movieSlice=createSlice({
 
     name:'Movie',
     initialState:[
-        {id:uuidv4(),title: 'John Wick: Chapitre 4 ', genre:'Action',checked:false,
+        {id:uuidv4(),title: 'John Wick: Chapitre 4 ', 
+        genre:'Action',checked:false,
     description:`John Wick: Chapitre 4 est un film d'action
      américain réalisé par Chad Stahelski et sorti en 2023.`,
     rate:7.8,sortie:2023,
     imageSrc:'./images/film1.jpg',
     videoUrl:'https://tse2.mm.bing.net/th?id=OMB2.1IRRxpPEEGxHlQ&pid=15.1'},
 
-    {id:uuidv4(),title:'La Petite Sirène ' ,genre:'fantastique musical',checked:false,
+    {id:uuidv4(),title:'La Petite Sirène ' ,
+    genre:'fantastique musical',checked:false,
     description:'La Petite Sirène est un film musical fantastique américain réalisé par Rob Marshall et sorti en 2023.',
     rate:7.2,sortie:2023,
     imageSrc:'./images/film2.jpg',
     videoUrl:'https://tse4.mm.bing.net/th?id=OMB2.kqhD1j12nYlzKQ&pid=15.1'},
  
-    {id:uuidv4(),title:'Fast and Furious 10' ,genre:'Action',checked:false,
+    {id:uuidv4(),title:'Fast and Furious 10' ,
+    genre:'Action',checked:false,
     description:'Fast and Furious 10 ou Rapides X au Québec (Fast X) est un film action américain réalisé par Louis Leterrier et sorti en 2023',
     rate:2.9,sortie:2023,
     imageSrc:'./images/film3.jpg',
     videoUrl:'https://tse2.mm.bing.net/th?id=OMB2.1IRRxpPEEGxHlQ&pid=15.1'},
 
-    {id:uuidv4(),title: 'Oppenheimer',genre:'Action',checked:false,
+    {id:uuidv4(),title: 'Oppenheimer',
+    
+    genre:'Action',checked:true,
     description:'The story of American scientist, J. Robert Oppenheimer, and his role in the development of the atomic bomb.',
     rate:8.5,sortie:2023,
     imageSrc:'./images/film4.jpg',
     videoUrl:'https://tse2.mm.bing.net/th?id=OMB2.1IRRxpPEEGxHlQ&pid=15.1'},
 
-    {id:uuidv4(),title:'Tyler Rake 2' ,genre:'Action',checked:false,
+    {id:uuidv4(),title:'Tyler Rake 2' ,
+    genre:'Action',checked:false,
       description:'Ayant échappé de peu à la mort, intrépide mercenaire Tyler Rake revient pour une nouvelle mission périlleuse : extraire de prison la famille un impitoyable gangster.',
       rate:6.5,sortie:2023,
       imageSrc:'./images/film5.jpg',
       videoUrl:'https://tse2.mm.bing.net/th?id=OMB2.1IRRxpPEEGxHlQ&pid=15.1'},
 
-    {id:uuidv4(),title:'Equalizer 3' ,genre:'Action',checked:false,
+    {id:uuidv4(),title:'Equalizer 3' ,
+    genre:'Action',checked:true,
     description:'Equalizer 3 ou Le justicier 3 au Québec, est un film action américain réalisé par Antoine Fuqua et sorti en 2023.',
     rate:7,sortie:2023,
     imageSrc:'./images/film6.jpg',
